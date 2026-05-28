979dd70b85138ee48f68f3f36d1e4bcaef7fa698
rollie-co <rv012502@gmail.com>
2026-05-28 19:20:51 +0530

    added factorial program

---
+++ Day 1/factorial_of_number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<<"enter the number jiska factorial chahiy -> \n";
+    cin>>n;
+    
+    int factorial = 1;
+    for(int i=1; i<=n; i++){
+        factorial= factorial*i;
+    }
+   cout<< "factorial of "<< n << " is " << factorial<< endl;
+}
