97c73030745bc8f155bd7e73ba5c6ec67d627043
rollie-co <rv012502@gmail.com>
2026-06-03 13:14:17 +0530

    checks number is prime

---
+++ Day 3/checking_prime_number.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter "<<endl;
+    cin>>n;
+    
+    bool isprime=1;
+    
+    for(int i = 2; i<n; i++){
+        if(n%i==0){
+            isprime=0;
+            break;
+        }
+      
+    }
+ 
+    if(isprime==0){
+        cout<<"this is not prime "<<endl;
+    }
+  else  
+  cout<<"this is prime "<<endl;
+
+}
