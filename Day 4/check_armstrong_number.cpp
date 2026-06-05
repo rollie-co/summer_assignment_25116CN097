2eb77eb4dab1da32e4ceabffc11912337eb734b8
rollie-co <rv012502@gmail.com>
2026-06-05 14:11:45 +0530

    can work for n numbers

--- Day 4/check_armstrong_number.cpp
+++ Day 4/check_armstrong_number.cpp
@@ -1,24 +1,38 @@
 #include<iostream>
 using namespace std;
+
+int power(int base, int exp){
+    int result = 1;
+    for(int i =0; i<exp; i++){
+       result= result * base;
+    }
+    return result;
+}
 int main (){
-    int n;
-    cout<<"enter the number to check armstrong"<<endl;
+    int n,count=0;
+    cout<<"enter the number to "<<endl;
     cin>>n;
 
     int original = n;
+    int temp = n;
     int sum = 0;
 
-while(n>0){
-    int rem= n%10;
-    n = n/10;
-    sum = sum + (rem*rem*rem);
+while(temp>0){
+    temp = temp/10;
+    count++;
 }
-
+temp = n;
+while(temp>0){
+ int rem= temp%10;
+  temp = temp/10;
+  sum = sum + power(rem, count);
+}
 if(sum==original){
-    cout<<" yes its armstrong number "<<endl;
+    cout<<"yeaaa it's a prime !!!"<<endl;
 }
 else
-cout<<" nahh its not "<<endl;
+cout<<"nahh try again "<<endl;
+
 }
 
 
