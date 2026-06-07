166f23699787d009bb703b7b0f926bc31ed26bd0
rollie-co <rv012502@gmail.com>
2026-06-07 20:34:41 +0530

    decimal to binary

---
+++ Day 6/decimal_to_binary.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"enter decimal number \n";
+    cin>>n;
+
+    int i =0;
+    int ans =0;
+while(n!=0){
+   int bit = n & 1;
+    
+  ans = ( bit * pow(10,i)) + ans;
+    
+    n = n >>1  ; 
+    i++;
+
+}
+ cout<<"the binary number is ->\n"<<ans<<endl;
+}
