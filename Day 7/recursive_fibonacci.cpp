9e2f35411fba4b886916215743382f94598c5db8
rollie-co <rv012502@gmail.com>
2026-06-11 18:19:32 +0530

    fibo digits by recursion

---
+++ Day 7/recursive_fibonacci.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int fibo(int n){
+    
+  // base case
+  if(n==0) {
+    return 0;
+  }
+    if( n==1){
+  return 1;
+  }
+////// processing...
+  int ans = fibo(n-1) + fibo(n-2);
+  return ans ;
+}
+int main (){
+    int n ;
+    cout<<"enter the n' term to find fibonacci"<<endl;
+    cin>>n;
+
+    cout<<"the "<<n<<" term is = "<<fibo(n)<<endl;
+}
