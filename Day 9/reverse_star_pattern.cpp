a176e80e85b983e1d7a9b8185088aa333eb08a29
rollie-co <rv012502@gmail.com>
2026-06-12 22:29:03 +0530

    reverse star pattern

---
+++ Day 9/reverse_star_pattern.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;                                                         // output ->
+int main(){                                                                  // *****
+ for( int i = 1; i<=5; i++){                                                 // ****
+    for(int j = i; j<=5; j++){                                               // ***
+        cout<<"*";                                                           // **
+    }                                                                        // *
+    cout<<endl;
+ }
+}
