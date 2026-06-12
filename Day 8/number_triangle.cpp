11d1fb53cd4dafda5b6b04c5d2e093d65759e435
rollie-co <rv012502@gmail.com>
2026-06-12 22:06:30 +0530

    number triangle

--- Day 8/number_triangle.cpp
+++ Day 8/number_triangle.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;                                                // output ->
-int main(){                                                            1 
- int n = 5;                                                            12
-    for(int i = 1; i<=n; i++){                                         123
-        for(int j= 1; j<=i; j++){                                      1234
-            cout<<j;                                                   12345
+int main(){                                                         //  1 
+ int n = 5;                                                         //  12
+    for(int i = 1; i<=n; i++){                                      //  123
+        for(int j= 1; j<=i; j++){                                   //  1234
+            cout<<j;                                                //  12345
         } 
         cout<<endl;
     }
