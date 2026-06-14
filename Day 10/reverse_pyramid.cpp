c775e32ff94899610d515246ee50294b24900e7b
rollie-co <rv012502@gmail.com>
2026-06-14 21:00:59 +0530

    reverse pyramid

---
+++ Day 10/reverse_pyramid.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;                                                      // output ->
+int main(){                                                             //  *******
+int n = 4;                                                              //   *****
+    for(int i =1 ; i<=n; i++){                                         //     ***
+        // star                                                        //      *
+        for(int j =1; j<=i; j++){                                   
+                cout<<" ";
+            }        
+        for(int j = 1; j<=2*(n-i)+1; j++){
+            cout<<"*";
+        }
+            cout<<endl;                                        
+        }                                                             
+        }
+    
+    
