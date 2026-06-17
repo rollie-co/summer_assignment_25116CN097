9bd847447affeb3e7d6ad268f5040f8531395f8a
rollie-co <rv012502@gmail.com>
2026-06-17 21:59:32 +0530

    func to find max

---
+++ Day 11/func_to_find_max.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int getMAX(int arr[], int n){
+   int max = INT_MIN;
+
+for(int i = 0 ;i<n ;i++){
+    if(arr[i]>max){
+        max = arr[i];
+    }
+}
+return max;
+}
+int main(){
+    int n ;
+    cout<<"enter the size of array\n";
+    cin>>n;
+
+    int arr[100];
+    cout<<"enter the elements of array\n";
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+     cout<<endl;
+    cout<<"the largest element is -> "<<getMAX(arr,n);
+
+}
